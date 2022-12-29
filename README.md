ba1193e0a04e0d40af1c824f1533326905347121
Greg Stretton <greg.stretton@protonmail.com>
2022-12-29 08:03:29 +0000

    Finish proto builder for c, go, and python

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # asol-protos
+
+This repo contains protos and build code for the protos.
+
+## Usage
+
+View / edit protos in proto/
+
+Build with
+
+```bash
+docker-compose up 
+```
--- build.sh
+++ build.sh
@@ -1,9 +1,43 @@
 NANOPB_VERSION=0.4.6
+NANOPB_ROOT_DIR=/opt/nanopb-$NANOPB_VERSION-linux-x86
 
-cd proto
+PROTO_PATH=./proto
+C_OUT_DIR=../c
+GO_OUT_DIR=../go
+PYTHON_OUT_DIR=../python
+GO_MODULE=github.com/gkstretton/asol-protos/go/machinepb
 
-/opt/nanopb-$NANOPB_VERSION-linux-x86/generator-bin/protoc \
-	--nanopb_out ../c/ \
+cd $PROTO_PATH
+
+mkdir -p $C_OUT_DIR
+mkdir -p $GO_OUT_DIR
+mkdir -p $PYTHON_OUT_DIR
+
+###### C  ######
+
+# Copy nanopb library files 
+mkdir -p $C_OUT_DIR/nanopb
+cp $NANOPB_ROOT_DIR/pb* $C_OUT_DIR/nanopb
+
+# Build protos for nanopb
+mkdir -p $C_OUT_DIR/machinepb
+$NANOPB_ROOT_DIR/generator-bin/protoc \
+	--nanopb_out $C_OUT_DIR/machinepb \
 	machine.proto
 
-sleep infinity
+###### GO ######
+
+# Build protos for go
+mkdir -p $GO_OUT_DIR/machinepb
+protoc --go_out $GO_OUT_DIR/machinepb --go_opt=module=$GO_MODULE ./machine.proto
+
+cd $GO_OUT_DIR
+go mod init $GO_MODULE
+go mod tidy
+
+cd ..
+cd $PROTO_PATH
+
+###### PYTHON ######
+mkdir -p $PYTHON_OUT_DIR/machinepb
+protoc --python_out $PYTHON_OUT_DIR/machinepb ./machine.proto
